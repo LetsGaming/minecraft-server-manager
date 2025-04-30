484d53bd9ee8052f5ea40b9486ba375da4afe7c1
LetsGaming <bergmanncrafter@gmail.com>
2025-04-30 22:34:26 +0200

    fix(ui): remove unused logToggleContainer from updateLogsView function

--- public/assets/js/utils.js
+++ public/assets/js/utils.js
@@ -1,12 +1,10 @@
 export function updateLogsView(show, updateToggle = true) {
-  const logToggleContainer = document.getElementById("log-toggle");
   const logToggleButton = document.getElementById("log-toggle-button");
   const logOutput = document.getElementById("log-output");
   const logControls = document.querySelectorAll(".log-control-inputs");
   const terminalContainer = document.getElementById("terminal-container");
 
   window.HIDE_LOGS = !show;
-  logToggleContainer.style.display = show ? "block" : "none";
   logOutput.style.display = show ? "block" : "none";
   terminalContainer.style.display = show ? "none" : "block";
   logControls.forEach((el) => (el.style.display = show ? "block" : "none"));
