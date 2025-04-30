b175ee8364f03ec0155c54a4240ce059cb8ee65b
LetsGaming <bergmanncrafter@gmail.com>
2025-04-30 22:26:24 +0200

    fixed typo in utils.js

--- public/assets/js/utils.js
+++ public/assets/js/utils.js
@@ -1,14 +1,14 @@
-exports.updateLogsView = function (show) {
-    const logToggleContainer = document.getElementById("log-toggle");
-    const logToggleButton = document.getElementById("log-toggle-button");
-    const logOutput = document.getElementById("log-output");
-    const logControls = document.querySelectorAll(".log-control-inputs");
-    const terminalContainer = document.getElementById("terminal-container");
-  
-    window.HIDE_LOGS = !show;
-    logToggleContainer.style.display = show ? "block" : "none";
-    logToggleButton.checked = show;
-    logOutput.style.display = show ? "block" : "none";
-    terminalContainer.style.display = show ? "none" : "block";
-    logControls.forEach((el) => (el.style.display = show ? "block" : "none"));
-  }
+export function updateLogsView(show) {
+  const logToggleContainer = document.getElementById("log-toggle");
+  const logToggleButton = document.getElementById("log-toggle-button");
+  const logOutput = document.getElementById("log-output");
+  const logControls = document.querySelectorAll(".log-control-inputs");
+  const terminalContainer = document.getElementById("terminal-container");
+
+  window.HIDE_LOGS = !show;
+  logToggleContainer.style.display = show ? "block" : "none";
+  logToggleButton.checked = show;
+  logOutput.style.display = show ? "block" : "none";
+  terminalContainer.style.display = show ? "none" : "block";
+  logControls.forEach((el) => (el.style.display = show ? "block" : "none"));
+}
